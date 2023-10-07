f6d574dca1ae19f55ea9559b4e2a8006506bca74
mauveyone <lennart@muenter.eu>
2023-10-07 23:20:43 +0200

    New Design for create new item page and addition of header on home

--- app/_layout.js
+++ app/_layout.js
@@ -1,15 +1,17 @@
 import {Stack} from 'expo-router';
 import * as SplashScreen from "expo-splash-screen";
-import {SafeAreaView} from "react-native";
+import {View} from "react-native";
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 SplashScreen.preventAutoHideAsync();
-
 const Layout = () => {
 
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+
     return (
-        <SafeAreaView className="h-full w-full bg-neutral-900">
+        <View className="h-full w-full">
             <Stack />
-        </SafeAreaView>
+        </View>
     );
 }
 
